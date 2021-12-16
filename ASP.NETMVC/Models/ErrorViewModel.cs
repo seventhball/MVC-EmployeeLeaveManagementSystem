bd7e1c64fb9f535bf81ab26272b8e15b4a82d5b5
seventhball <sameersrivastava8055@gmail.com>
2021-12-16 14:15:57 +0530

    initial commit

---
+++ ASP.NETMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NETMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
